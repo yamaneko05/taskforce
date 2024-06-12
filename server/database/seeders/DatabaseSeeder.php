196fe87391cb97a3cead73b73953d98dc2a1080c
yamaneko05 <o.yamaneko0331@gmail.com>
2024-06-12 10:21:22 +0900

    seeder and default storage directory

--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
+use App\Models\Project;
+use App\Models\Role;
+use App\Models\Section;
+use App\Models\Task;
+use App\Models\Team;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +19,50 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        # users
+        $userAdmin = User::factory()->make();
+        $userAdmin->name = '管理者ユーザー';
+        $userAdmin->email = 'admin@example.com';
+        $userAdmin->save();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $userGeneral = User::factory()->make();
+        $userGeneral->name = '一般ユーザー';
+        $userGeneral->email = 'general@example.com';
+        $userGeneral->save();
+
+        # team
+        $team = new Team();
+        $team->title = 'テストチーム';
+        $team->save();
+        
+        # team_user
+        $userAdmin->teams()->attach($team->id, ['role' => \App\Enums\Role::Admin]);
+        $userGeneral->teams()->attach($team->id, ['role' => \App\Enums\Role::General]);
+
+        # projects
+        $project = new Project();
+        $project->title = 'テストプロジェクト';
+        $project->color = \App\Enums\ProjectColor::Blue;
+        $team->projects()->save($project);
+
+        # sections
+        $section = new Section();
+        $section->title = 'テストセクション';
+        $project->sections()->save($section);
+
+        # tasks
+        $task = new Task();
+        $task->title = 'テストタスク';
+        $task->status = \App\Enums\TaskStatus::Todo;
+        $section->tasks()->save($task);
+
+        # task_user
+        $task->users()->attach($userGeneral->id);
+
+        # comment
+        $comment = new Comment();
+        $comment->text = 'テストコメント';
+        $comment->user_id = $userGeneral->id;
+        $task->comments()->save($comment);
     }
 }
